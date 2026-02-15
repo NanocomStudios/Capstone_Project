fbf36b7d076b0f5870ed71ca74e8b6b3a48a0723
NanocomStudios <dulararupasinghe@gmail.com>
2026-02-15 10:17:44 +0530

    Adapter Service

--- app/api/wms_adapter/wms_adapter.py
+++ app/api/wms_adapter/wms_adapter.py
@@ -1,102 +1,54 @@
 from fastapi import FastAPI
-import socket
+from app.services.wms_adapter.wms_adapter import WMSAdapter
 
 wms = FastAPI()
-s = None
-    
+adapter = WMSAdapter()
+
 @wms.get("/connect/{ip}/{port}")
 def connect_to_server(ip: str, port: int):
-    global s
-    s = socket.socket()
-    try:
-        s.connect((ip, port))
-    except Exception as e:
-        s = None
-        return {"response": f"Failed to connect to server at {ip}:{port}", "error": str(e)}
-    return {"response": f"Connected to server at {ip}:{port}", "server_response": s.recv(1024).decode()}
+    return adapter.connect(ip,port)
 
 @wms.get("/disconnect")
 def disconnect_from_server():
-    global s
-    if s is None:
-        return {"response": "Not connected to server"}
-    s.send("exit\n".encode('ascii'))
-    response = recvAll(s)
-    s.close()
-    s = None
-    return {"response": f"Disconnected from server", "server_response": response}
+    try:
+        return adapter.disconnect()
+    except Exception as e:
+        return {"response": f"Error", "error": str(e)}
+    
 
 @wms.get("/add")
 def add_item():
-    if s is None:
-        return {"response": "Not connected to server"}
-    s.send("add\n".encode('ascii'))
-    response = recvAll(s)
-    return ssvToDict(response)
+    try:
+        return adapter.add_item()
+    except Exception as e:
+        return {"response": f"Error", "error": str(e)}
 
 @wms.get("/pack/{item_id}")
 def pack_item(item_id: int):
-    if s is None:
-        return {"response": "Not connected to server"}
-    s.send(f"pack {item_id}\n".encode('ascii'))
-    response = recvAll(s)
-    return ssvToDict(response)
+    try:
+        return adapter.pack_item(item_id)
+    except Exception as e:
+        return {"response": f"Error", "error": str(e)}
 
 @wms.get("/ship/{item_id}")
 def ship_item(item_id: int):
-    if s is None:
-        return {"response": "Not connected to server"}
-    s.send(f"ship {item_id}\n".encode('ascii'))
-    response = recvAll(s)
-    return ssvToDict(response)
+    try:
+        return adapter.ship_item(item_id)
+    except Exception as e:
+        return {"response": f"Error", "error": str(e)}
 
 @wms.get("/state/{item_id}")
 def get_item_state(item_id: int):
-    if s is None:
-        return {"response": "Not connected to server"}
-    s.send(f"state {item_id}\n".encode('ascii'))
-    response = recvAll(s)
-    return ssvToDict(response)
+    try:
+        return adapter.get_item_state(item_id)
+    except Exception as e:
+        return {"response": f"Error", "error": str(e)}
 
 @wms.get("/list")
 def list_items():
-    if s is None:
-        return {"response": "Not connected to server"}
-    s.send("list\n".encode('ascii'))
-    response = recvAll(s)
-    return ssvToDict(response)
+    try:
+        return adapter.list_items()
+    except Exception as e:
+        return {"response": f"Error", "error": str(e)}
 
-def ssvToDict(ssv_string):
-    lines = ssv_string.split("\r\n")
-    out = {"response": []}
 
-    for line in lines:
-        if len(line.strip()) == 0:
-            continue
-
-        words = line.split(' ')
-        if len(words) == 0:
-            continue
-        elif len(words) == 1:
-            out["response"].append({words[0]: ""})
-        else:
-            out["response"].append({words[0]: words[1]})
-    return out
-
-def recvAll(sock):
-    data = b""
-    while True:
-        part = sock.recv(1024)
-        data += part
-        if len(part) < 1024:
-            break
-    return data.decode()
-    
-
-# Legacy_Server_IP = "127.0.0.1"
-# Legacy_Server_PORT = 5000
-
-# s = socket.socket()
-# s.connect((Legacy_Server_IP, Legacy_Server_PORT))
-
-# print(s.recv(1024).decode())
---
+++ app/services/wms_adapter/wms_adapter.py
@@ -0,0 +1,84 @@
+import socket
+
+class WMSAdapter:
+    def __init__(self):
+        self.s = None
+
+    def connect(self, ip: str, port: int):
+        self.s = socket.socket()
+        try:
+            self.s.connect((ip, port))
+        except Exception as e:
+            self.s = None
+            return {"response": f"Failed to connect to server at {ip}:{port}", "error": str(e)}
+        return {"response": f"Connected to server at {ip}:{port}", "server_response": self.s.recv(1024).decode()}
+
+    def disconnect(self):
+        if self.s is None:
+            return {"response": "Not connected to server"}
+        self.s.send("exit\n".encode('ascii'))
+        response = self.recvAll(self.s)
+        self.s.close()
+        self.s = None
+        return {"response": f"Disconnected from server", "server_response": response}
+
+    def add_item(self):
+        if self.s is None:
+            return {"response": "Not connected to server"}
+        self.s.send("add\n".encode('ascii'))
+        response = self.recvAll(self.s)
+        return self.ssvToDict(response)
+
+    def pack_item(self, item_id: int):
+        if self.s is None:
+            return {"response": "Not connected to server"}
+        self.s.send(f"pack {item_id}\n".encode('ascii'))
+        response = self.recvAll(self.s)
+        return self.ssvToDict(response)
+
+    def ship_item(self, item_id: int):
+        if self.s is None:
+            return {"response": "Not connected to server"}
+        self.s.send(f"ship {item_id}\n".encode('ascii'))
+        response = self.recvAll(self.s)
+        return self.ssvToDict(response)
+
+    def get_item_state(self, item_id: int):
+        if self.s is None:
+            return {"response": "Not connected to server"}
+        self.s.send(f"state {item_id}\n".encode('ascii'))
+        response = self.recvAll(self.s)
+        return self.ssvToDict(response)
+    
+    def list_items(self):
+        if self.s is None:
+            return {"response": "Not connected to server"}
+        self.s.send("list\n".encode('ascii'))
+        response = self.recvAll(self.s)
+        return self.ssvToDict(response)
+    
+    def ssvToDict(self, ssv_string):
+        lines = ssv_string.split("\r\n")
+        out = {"response": []}
+
+        for line in lines:
+            if len(line.strip()) == 0:
+                continue
+
+            words = line.split(' ')
+            if len(words) == 0:
+                continue
+            elif len(words) == 1:
+                out["response"].append({words[0]: ""})
+            else:
+                out["response"].append({words[0]: words[1]})
+        return out
+
+    def recvAll(self, sock):
+        data = b""
+        while True:
+            part = sock.recv(1024)
+            data += part
+            if len(part) < 1024:
+                break
+        return data.decode()
