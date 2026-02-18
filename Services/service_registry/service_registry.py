0058ce32f2b4b51850eb33f9f1c72e9ab1d19d61
NanocomStudios <dulararupasinghe@gmail.com>
2026-02-18 16:31:22 +0530

    docker

---
+++ Services/service_registry/service_registry.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Dict
+import time
+
+app = FastAPI(title="SwiftLogistics Service Registry")
+
+registry: Dict[str, dict] = {}
+
+class ServiceSchema(BaseModel):
+    name: str
+    address: str  #like "http://192.168.1.50:8000"
+
+@app.post("/register")
+def register_service(service: ServiceSchema):
+    registry[service.name] = {
+        "address": service.address,
+        "last_heartbeat": time.time()
+    }
+    print(f"Registered {service.name} at {service.address}")
+    return {"status": "registered"}
+
+@app.get("/discover/{service_name}")
+def discover_service(service_name: str):
+    if service_name not in registry:
+        raise HTTPException(status_code=404, detail="Service not found")
+    
+    ## if the last heartbeat was more than 30 seconds ago, consider the service dead
+    # if time.time() - registry[service_name]["last_heartbeat"] > 30:
+    #     del registry[service_name]
+    #     raise HTTPException(status_code=404, detail="Service timed out")
+        
+    return {"address": registry[service_name]["address"]}
+
+@app.get("/health")
+def health():
+    return {"services_online": list(registry.keys())}
